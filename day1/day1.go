0038d4b6c617f04e565e0581214edac38f277929
farshid.besharati <farshid@besharati.se>
2022-12-04 22:01:15 +0100

    Day 3

---
+++ day1/day1.go
@@ -0,0 +1,35 @@
+package day1
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+
+	utils "besharati.se/advent-of-code-2022/utils"
+)
+
+func buildElves() []int {
+	input := utils.ReadFile("input_1_1.txt")
+
+	elves := []int{}
+	elfItems := []int{}
+	for _, txt := range input {
+		if txt == "" {
+			elves = append(elves, utils.SumIntSlice(elfItems))
+			elfItems = []int{}
+			continue
+		}
+
+		val, err := strconv.Atoi(txt)
+		utils.Check(err)
+		elfItems = append(elfItems, val)
+	}
+	return elves
+}
+
+func day1() {
+	elves := buildElves()
+	sort.Ints(elves)
+	fmt.Println(elves[len(elves)-1:][0])
+	fmt.Println(utils.SumIntSlice(elves[len(elves)-3:]))
+}
--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,35 @@
 package utils
 
+import (
+	"bufio"
+	"os"
+)
+
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func SumIntSlice(slice []int) int {
+	total := 0
+	for _, s := range slice {
+		total += s
+	}
+	return total
+}
+
+func ReadFile(fileName string) []string {
+	input, err := os.Open(fileName)
+	Check(err)
+	defer input.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(input)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
 }
